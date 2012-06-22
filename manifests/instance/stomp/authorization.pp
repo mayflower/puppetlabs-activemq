b831ffefd928af7fd1247e7e81ba921901f66f36
Adrien Thebo <adrien@puppetlabs.com>
2012-06-22 13:14:59 -0700

    Fix template errors

--- manifests/instance/stomp/authorization.pp
+++ manifests/instance/stomp/authorization.pp
@@ -3,10 +3,10 @@ define activemq::instance::stomp::authorization($type, $content, $read, $write, 
   include activemq::params
 
   $instance_xml = "${activemq::params::amq_instancedir}/${target_instance}/activemq.xml"
-  $data = inline_template("                  <authorizationEntry <% @type %>=\"<%= content %>\" write=\"<%= write %>\" read=\"<%= @read %>\" admin=\"<%= admin %>\" />\n")
+  $data = inline_template("                  <authorizationEntry <%= @type %>=\"<%= content %>\" write=\"<%= write %>\" read=\"<%= @read %>\" admin=\"<%= admin %>\" />\n")
 
   concat::fragment { "${target_instance}-authorization-${name}":
-    content => $data,
+    content => "${data}\n",
     order   => '350',
     target  => $instance_xml,
   }
--- manifests/instance/stomp/user.pp
+++ manifests/instance/stomp/user.pp
@@ -3,10 +3,10 @@ define activemq::instance::stomp::user($username, $password, $groups, $target_in
   include activemq::params
 
   $instance_xml = "${activemq::params::amq_instancedir}/${target_instance}/activemq.xml"
-  $data = inline_template("              <authenticationUser username=\"<%= username %>\" password=\"<%= password %>\" groups=\"<%= groups.join(',') %>\"/>\n")
+  $data = inline_template("              <authenticationUser username=\"<%= username %>\" password=\"<%= password %>\" groups=\"<%= groups.join(',') %>\"/>")
 
   concat::fragment { "${target_instance}-user-${name}":
-    content => $data,
+    content => "${data}\n",
     order   => '250',
     target  => $instance_xml,
   }
