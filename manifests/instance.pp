0550070ff7a51607ffc54fc0c7033581ebe6ff54
Adrien Thebo <adrien@puppetlabs.com>
2012-06-22 11:52:55 -0700

    Rewrote activemq::instance

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,28 +1,18 @@
 # Generic activemq instance define.
-define activemq::instance($template, $template_options = {}) {
+define activemq::instance {
 
   include activemq
   include activemq::params
 
   $instance_dir     = "${activemq::params::amq_instancedir}/${name}"
-  $instance_xml     = "${activemq::params::amq_instancedir}/${name}/activemq.xml"
-  $instance_logging = "${activemq::params::amq_instancedir}/${name}/log4j.properties"
-  $instance_options = "${activemq::params::amq_instancedir}/${name}/options"
+  $instance_xml     = "${instance_dir}/activemq.xml"
+  $instance_logging = "${instance_dir}/log4j.properties"
+  $instance_options = "${instance_dir}/options"
 
   file { $instance_dir:
     ensure => directory,
     owner   => 'root',
     group   => 'root',
-  }
-
-  file { $instance_xml:
-    ensure  => file,
-    content => template($template),
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => Class['activemq::pkg'],
-    notify  => Class['activemq::service'],
   }
 
   file { $instance_logging:
@@ -43,5 +33,32 @@ define activemq::instance($template, $template_options = {}) {
     group   => 'root',
     require => Class['activemq::pkg'],
     notify  => Class['activemq::service'],
+  }
+
+  # ---
+  # Build our activemq.xml out of itsy bitsy little chunks so that
+  # functionality like users and protocols can be broken out into other 
+  # defines.
+
+  concat { $instance_xml:
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => Class['activemq::pkg'],
+    notify  => Class['activemq::service'],
+  }
+
+  # Add the XML header
+  concat::fragment { "${name}-header":
+    target  => $instance_xml,
+    content => template('activemq/activemq.xml/header.xml.erb'),
+    order   => '000',
+  }
+
+  # Close the XML document
+  concat::fragment { "${name}-footer":
+    target  => $instance_xml,
+    content => template('activemq/activemq.xml/footer.xml.erb'),
+    order   => '999',
   }
 }
