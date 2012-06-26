e45f63f34742017f9973e46dbc16274019ed313d
Adrien Thebo <adrien@puppetlabs.com>
2012-06-26 10:50:22 -0700

    Use genpass in activemq for the keystore

--- manifests/instance/ssl.pp
+++ manifests/instance/ssl.pp
@@ -2,11 +2,13 @@ class activemq::instance::ssl {
 
   include activemq::params
 
+  $passwd = genpass({store_key => 'activemq_store'})
+
   java_ks { 'puppetca:truststore':
     ensure       => latest,
     certificate  => "${::settings::ssldir}/certs/ca.pem",
     target       => "${::activemq::params::amq_configdir}/broker.ts",
-    password     => 'lolthisisfake', # REALLY?!?!?!
+    password     => $passwd,
     trustcacerts => true,
   }
 
@@ -15,7 +17,7 @@ class activemq::instance::ssl {
     certificate  => "${::settings::ssldir}/certs/${::clientcert}.pem",
     private_key  => "${::settings::ssldir}/private_keys/${::clientcert}.pem",
     target       => "${::activemq::params::amq_configdir}/broker.ks",
-    password     => 'lolthisisfake', # REALLY?!?!?!
+    password     => $passwd,
     trustcacerts => true,
   }
 }
