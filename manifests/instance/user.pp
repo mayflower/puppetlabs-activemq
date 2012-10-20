54a88e343f513f75335c3d13fcaa6065aac1e0e0
Adrien Thebo <adrien@puppetlabs.com>
2012-10-19 20:07:50 -0700

    Better arg handling for activemq::instance::user
    Groups can be either a string or array.

--- manifests/instance/user.pp
+++ manifests/instance/user.pp
@@ -2,8 +2,13 @@ define activemq::instance::user($username, $password, $groups, $target_instance)
 
   include activemq::params
 
+  $groups_str = is_string($groups) ? {
+    true  => $groups,
+    false => join($groups, ','),
+  }
+
   $instance_xml = "${activemq::params::amq_instancedir}/${target_instance}/activemq.xml"
-  $data = inline_template("              <authenticationUser username=\"<%= username %>\" password=\"<%= password %>\" groups=\"<%= groups.join(',') %>\"/>\n")
+  $data = "              <authenticationUser username=\"${username}\" password=\"${password}\" groups=\"${groups_str}\"/>\n"
 
   concat::fragment { "${target_instance}-user-${name}":
     content => $data,
