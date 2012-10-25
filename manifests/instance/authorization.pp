1c16a8ecaeb95ceea57687c6d1b34b6215f6f9d5
Adrien Thebo <adrien@puppetlabs.com>
2012-10-25 11:08:29 -0700

    Add shortcut allperms parameter

--- manifests/instance/authorization.pp
+++ manifests/instance/authorization.pp
@@ -1,9 +1,32 @@
-define activemq::instance::authorization($type, $destination, $read, $write, $admin, $target_instance) {
+define activemq::instance::authorization(
+  $type,
+  $destination,
+  $target_instance,
+  $read            = undef,
+  $write           = undef,
+  $admin           = undef,
+  $allperms        = undef,
+) {
 
   include activemq::params
 
+  if !($read and $write and $admin) and !$allperms {
+    crit("Either all of [read, write, admin] or [allperms] must be specified")
+  }
+
+  if $allperms {
+    $read_real  = $allperms
+    $write_real = $allperms
+    $admin_real = $allperms
+  }
+  else {
+    $read_real  = $read
+    $write_real = $write
+    $admin_real = $admin
+  }
+
   $instance_xml = "${activemq::params::amq_instancedir}/${target_instance}/activemq.xml"
-  $data = inline_template("                  <authorizationEntry <%= @type %>=\"<%= destination %>\" write=\"<%= write %>\" read=\"<%= @read %>\" admin=\"<%= admin %>\" />\n")
+  $data = inline_template("                  <authorizationEntry <%= @type %>=\"<%= destination %>\" write=\"<%= write_real %>\" read=\"<%= read_real %>\" admin=\"<%= admin_real %>\" />\n")
 
   concat::fragment { "${target_instance}-authorization-${name}":
     content => $data,
