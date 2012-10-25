233b8991f3669788e239d1ae8829f43a874c8cfd
Adrien Thebo <adrien@puppetlabs.com>
2012-10-25 11:02:23 -0700

    rename authorization content to destination

--- manifests/instance/authorization.pp
+++ manifests/instance/authorization.pp
@@ -1,9 +1,9 @@
-define activemq::instance::authorization($type, $content, $read, $write, $admin, $target_instance) {
+define activemq::instance::authorization($type, $destination, $read, $write, $admin, $target_instance) {
 
   include activemq::params
 
   $instance_xml = "${activemq::params::amq_instancedir}/${target_instance}/activemq.xml"
-  $data = inline_template("                  <authorizationEntry <%= @type %>=\"<%= content %>\" write=\"<%= write %>\" read=\"<%= @read %>\" admin=\"<%= admin %>\" />\n")
+  $data = inline_template("                  <authorizationEntry <%= @type %>=\"<%= destination %>\" write=\"<%= write %>\" read=\"<%= @read %>\" admin=\"<%= admin %>\" />\n")
 
   concat::fragment { "${target_instance}-authorization-${name}":
     content => $data,
